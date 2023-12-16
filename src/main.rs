bfe04e7e7c5a6a88c22f19d492af3e52587432ff
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-12-16 16:50:23 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -75,9 +75,9 @@ fn main() {
     //queue
     let mut queue = Queue::new();
 
+    queue.enqueue(3);
     queue.enqueue(1);
     queue.enqueue(2);
-    queue.enqueue(3);
 
     queue.print_all();
     
