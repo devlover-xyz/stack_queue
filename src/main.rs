b1bfaeb5ccf9aaaa2ebb3da9f69a19fcd30cb91f
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-12-16 16:49:25 +0700

    upate queu

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
 use std::fmt::Debug;
 
-struct Stack<T> {
-    items: Vec<T>,
-}
-
-impl<T> Stack<T> {
-    // create a new empty stack
-    fn new() -> Self {
-        Stack { items: Vec::new() }
-    }
-
-    // push an item onto stack
-    fn push(&mut self, item: T) {
-        self.items.push(item);
-    }
-
-    fn pop(&mut self) -> Option<T> {
-        self.items.pop()
-    }
-}
-
 struct Queue<T> {
     items: Vec<T>,
 }
@@ -32,10 +12,12 @@ where
         Queue { items: Vec::new() }
     }
 
+    //menambah data
     fn enqueue(&mut self, item: T) {
         self.items.push(item);
     }
 
+    //menghapus data
     fn dequeue(&mut self) -> Option<T> {
         if self.is_empty() {
             None
@@ -44,6 +26,7 @@ where
         }
     }
 
+    // cek apakah kosong
     fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
@@ -56,28 +39,13 @@ where
 }
 
 fn main() {
-    //stack
-    let mut stack = Stack::new();
-
-    stack.push(3);
-    stack.push(1);
-    stack.push(2);
-
-    // Display the data in the stack before popping
-    if let Some(top) = stack.pop() {
-        println!("Top of the stack: {}", top);
-    }
-
-    while let Some(item) = stack.pop() {
-        println!("Proped from stack: {}", item);
-    }
-
+    
     //queue
     let mut queue = Queue::new();
 
+    queue.enqueue(3);
     queue.enqueue(1);
     queue.enqueue(2);
-    queue.enqueue(3);
 
     queue.print_all();
     
