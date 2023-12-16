f06bbdc037f6522e77324ef5ca49a42c24238b49
aviq baihaqy <aviq.baihaqy@gmail.com>
2023-12-16 16:43:30 +0700

    update stack

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,29 @@ struct Stack<T> {
     items: Vec<T>,
 }
 
-impl<T> Stack<T> {
-    // create a new empty stack
+impl<T> Stack<T>
+where
+    T: Debug,
+{
+    //new
     fn new() -> Self {
         Stack { items: Vec::new() }
     }
 
-    // push an item onto stack
+    //push
     fn push(&mut self, item: T) {
         self.items.push(item);
     }
 
+    //pop
     fn pop(&mut self) -> Option<T> {
         self.items.pop()
     }
-}
 
-struct Queue<T> {
-    items: Vec<T>,
-}
-
-impl<T> Queue<T>
-where
-    T: Debug,
-{
-    fn new() -> Self {
-        Queue { items: Vec::new() }
-    }
-
-    fn enqueue(&mut self, item: T) {
-        self.items.push(item);
-    }
-
-    fn dequeue(&mut self) -> Option<T> {
-        if self.is_empty() {
-            None
-        } else {
-            Some(self.items.remove(0))
-        }
-    }
-
-    fn is_empty(&self) -> bool {
-        self.items.is_empty()
-    }
-
+    //print all data
     fn print_all(&self) {
         for item in &self.items {
-            println!("Queue item: {:?}", item);
+            println!("Stack item: {:?}", item);
         }
     }
 }
@@ -63,6 +39,9 @@ fn main() {
     stack.push(1);
     stack.push(2);
 
+    //Display all data
+    stack.print_all();
+
     // Display the data in the stack before popping
     if let Some(top) = stack.pop() {
         println!("Top of the stack: {}", top);
@@ -70,18 +49,5 @@ fn main() {
 
     while let Some(item) = stack.pop() {
         println!("Proped from stack: {}", item);
-    }
-
-    //queue
-    let mut queue = Queue::new();
-
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-
-    queue.print_all();
-    
-    while let Some(item) = queue.dequeue() {
-        println!("Dequeue from queue: {}", item);
     }
 }
